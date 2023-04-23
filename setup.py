31ba96ce493983d4bb46b36624b78c58f743a70b
Geofrey Wanyama <geofrey.wanyama@monese.com>
2023-04-23 16:39:24 +0300

    typos

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
 
     # description='Fitting ols model in python',
 
-    packages=['gspWeb', 'gspWeb.econometrics'],
+    packages=find_packages()#['gspWeb', 'gspWeb.econometrics'],
 
     # directory in which code file is stored
     package_dir={'':'src'},
